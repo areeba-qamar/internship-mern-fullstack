19299bb1fed5dbb39cfdb13e95811aa48bdfc372
Areeba <areebaqamar2901@gmail.com>
2026-08-18 17:34:31 +0500

    Implement controlled search form and event handling

--- week-3/weather-app-react/src/App.jsx
+++ week-3/weather-app-react/src/App.jsx
@@ -1,16 +1,27 @@
 import SearchForm from "./components/SearchForm";
 import WeatherCard from "./components/WeatherCard";
+import { useState } from "react";
 
 function App() {
+  // State lives in parent → lifting state up
+  const [city, setCity] = useState("");
+
+  const handleSearch = (searchedCity) => {
+    setCity(searchedCity);
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">
+    <div className="min-h-screen p-8">
+      <h1 className="text-4xl font-bold text-centre">
         Weather Dashboard 🌦️
       </h1>
 
-      <SearchForm/>
+      <SearchForm onSearch={handleSearch}/>
+      <p className="text-center mt-5">
+        Searching for: {city}
+      </p>
       <WeatherCard/>
-      
+
     </div>
   );
 }
--- week-3/weather-app-react/src/components/SearchForm.jsx
+++ week-3/weather-app-react/src/components/SearchForm.jsx
@@ -1,8 +1,40 @@
-function SearchForm() {
+import { useState } from "react";
+
+function SearchForm({onSearch}) {
+
+     // useState → stores the current input value
+  const [city, setCity] = useState("");
+
+  // Event handler → handles form submission
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!city.trim()) {
+      return;
+    }
+
+    onSearch(city);
+  };
+
   return (
-    <form>
-      <input placeholder="Enter city" />
-      <button type="submit">Search</button>
+    <form
+     onSubmit={handleSubmit}
+      className="flex gap-3 justify-center mt-8">
+
+        {/* Controlled input */}
+
+      <input 
+      type="text"
+      value={city}
+      onChange={(event) => setCity(event.target.value)}
+      placeholder="Enter city"
+      className="border rounded-lg px-4 py-2"
+        />
+      <button 
+      type="submit"
+      className="bg-blue-600 text-white px-5 py-2 rounded-lg"
+      >Search
+      </button>
     </form>
   );
 }
